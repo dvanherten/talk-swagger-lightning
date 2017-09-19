13ac23ee093658e3eb82cb8f96e54ca55f3b9aa5
Dave van Herten <dvanherten@outlook.com>
2017-09-18 23:16:12 -0400

    Initial demo project structure

---
+++ SwaggerDemo/SwaggerDemo/Controllers/DemoController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SwaggerDemo.Controllers
+{
+    public class DemoModel
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+
+    [Route("api/[controller]")]
+    public class DemoController : Controller
+    {
+        private static readonly DemoModel[] _models = {
+            new DemoModel {Id = 1, Text = "Hi Mississauga .NET User Group!"},
+            new DemoModel {Id = 2, Text = "Demo by Dave van Herten"}
+        };
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<DemoModel> Get()
+        {
+            return _models;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var model = _models.FirstOrDefault(_ => _.Id == id);
+            if (model == null)
+                return NotFound();
+            return Ok(model);
+        }
+    }
+}
