0e984f95bae2911a18cd8ed5c0249f48f4c240b2
Dave van Herten <dvanherten@outlook.com>
2017-12-05 22:47:13 -0500

    well how about that. I did it.

--- PhpDemo/app/Http/Controllers/DemoController.php
+++ PhpDemo/app/Http/Controllers/DemoController.php
@@ -7,14 +7,64 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use App\Demo;
 use Exception;
 
+/**
+ * Class DemoController
+ *
+ * @package App\Http\Controllers
+ */
 class DemoController extends Controller
 {
+    /**
+     * Display a listing of our demo items.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     *
+     * @SWG\Get(
+     *     path="/api/demo",
+     *     description="Returns demo items.",
+     *     operationId="api.demo.index",
+     *     produces={"application/json"},
+     *     tags={"demo"},
+     *     @SWG\Response(
+     *         response=200,
+     *         description="Demo array."
+     *     )
+     * )
+     */
     public function index()
     {
         $demo = new Demo();
         return response()->json($demo->getExamples(), 200);
     }
 
+    /**
+     * Display a single demo item.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     *
+     * @SWG\Get(
+     *     path="/api/demo/{id}",
+     *     description="Returns demo overview.",
+     *     operationId="api.demo.index",
+     *     produces={"application/json"},
+     *     tags={"demo"},
+     *     @SWG\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="The id of the demo item to retrieve",
+     *         type="integer"
+     *     ),
+     *     @SWG\Response(
+     *         response=200,
+     *         description="A Demo item."
+     *     ),
+     *     @SWG\Response(
+     *         response=404,
+     *         description="Demo item missing."
+     *     ),
+     * )
+     */
     public function show($id)
     {
         $demo = new Demo();
